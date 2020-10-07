f255abcfe1c4633db8165373818ed3d71132a7dd
kartik <karthiksharma.16ec@saividya.ac.in>
2020-10-07 13:58:54 +0530

    First web app

---
+++ conFusion_nodejs/conFusionServer/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook'  : {
+        clientId : '975043772974857',
+        clientSecret : '24a3c0a7b1b88228561e6bac366acfdd'
+    }
+}
